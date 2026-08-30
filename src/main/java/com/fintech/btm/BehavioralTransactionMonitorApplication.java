3f4b154c23f64887b197991a4d7523e479ddbc87
Ivy-Mwende <mwendeivymumbi@gmail.com>
2026-08-30 17:20:23 +0300

    Day 1: Initial Spring Boot scaffold and project structure

--- src/main/java/com/fintech/btm/BehavioralTransactionMonitorApplication.java
+++ src/main/java/com/fintech/btm/BehavioralTransactionMonitorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BehavioralTransactionMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BehavioralTransactionMonitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BehavioralTransactionMonitorApplication.class, args);
+    }
 
 }
